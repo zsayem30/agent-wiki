d98651942ea4307804c50fe248582a90b8f14037
zsayem30 <sayemnazmuz@gmail.com>
2026-05-31 19:57:46 -0700

    Add sidebar logo variants

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
 
 templates_path = ["_templates"]
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = ["_static", "../images"]
 html_css_files = ["custom.css"]
 html_js_files = ["theme-toggle.js"]
 html_title = "agent-wiki"
